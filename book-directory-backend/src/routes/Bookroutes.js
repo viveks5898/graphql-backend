7126677cd7b29bc9556c68a1d422fa288cb64d05
viveks5898 <39966959+viveks5898@users.noreply.github.com>
2023-05-26 01:18:21 +0530

    Delete book-directory-backend directory

--- book-directory-backend/src/routes/Bookroutes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { BookController, getBookController } from "../controller/Bookcontroller.js";
-const router = Router();
-
-router.post("/createBook", BookController);
-router.get("/getBooks", getBookController);
-
-export default router;
